a26cb8eb233f15e51134ab93d6858ec42bd70e38
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2024-03-07 14:49:01 +0400

    fix: lang bug

--- src/js/lang.js
+++ src/js/lang.js
@@ -13,7 +13,7 @@ function checkBrowserLang() {
 }
 
 // Set the language when loading the page
-let currentLang = localStorage.getItem('language') || checkBrowserLang() || 'en';
+let currentLang = 'ru';
 const langBtns = document.querySelectorAll('[data-btn]');
 
 // Text for main page
